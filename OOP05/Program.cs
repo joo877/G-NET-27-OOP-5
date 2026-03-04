c95d66718fc18f234ebbc9af8d034ab09ec54350
Youssef said <jooasaid2004@gmail.com>
2026-03-04 14:39:55 +0400

    Q02

--- OOP05/Program.cs
+++ OOP05/Program.cs
@@ -16,6 +16,46 @@
              */
 
             #endregion
+            #region Q2
+            /*
+            A-  Problem: Method Name Conflict
+            when called Greet() method from Translator inistance same behavior in two interface 
+
+            B- To solve this problem using Explicit Implementation
+            
+            class Translator : IEnglishSpeaker, IArabicSpeaker
+               {
+                       void IEnglishSpeaker.Greet()
+                        {
+                       Console.WriteLine("Hello");
+                        }
+
+                        void IArabicSpeaker.Greet()
+                        {
+                       Console.WriteLine("Ahlan");
+                        }
+   
+             }
+
+
+
+            C- After applied Explicit Implementation  Cannot call  Greet() method  directly on a Translator object 
+             translator.Greet() //invalid because Greet() follow interface now not class (translator cannot be access Greet() method now)
+
+              IEnglishSpeaker english = new  Translator (); 
+              english.Greet();  // call Greet() method from  IEnglishSpeaker
+
+             IArabicSpeaker arabic = new  Translator (); 
+              arabic.Greet();  // call Greet() method from  IArabicSpeaker
+
+          //  also can cast directory 
+
+            Translator t = new Translator();
+                ((IEnglishSpeaker)t).Greet();
+                ((IArabicSpeaker)t).Greet();
+
+             */
+            #endregion
             #endregion
         }
     }
