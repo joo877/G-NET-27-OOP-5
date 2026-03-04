d1aa056c855d55664686765c42620aefb0e7cba5
Youssef said <jooasaid2004@gmail.com>
2026-03-04 14:21:32 +0400

    Q1

--- OOP05/Program.cs
+++ OOP05/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-          
+
+            #region part01
+            #region Q1
+            /*
+             An interface defines a contract that a class must follow
+
+            benefits :  1- Enable polymorphism without inheritance
+                        2- Remove tight coupling between classes
+                        3-Enable multiple inheritance (behavior)
+             */
+
+            #endregion
+            #endregion
         }
     }
 }
